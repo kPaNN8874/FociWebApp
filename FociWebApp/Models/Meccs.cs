9c4b87b05ae93e10a7aa57ca5399c4b8018352c1
kelemen.panna <kelemen.panna@BURG-1E05-17>
2024-09-16 08:42:44 +0200

    Class létrehozása

---
+++ FociWebApp/Models/Meccs.cs
@@ -0,0 +1,15 @@
+﻿namespace FociWebApp.Models
+{
+    public class Meccs
+    {
+        public int Id { get; set; } 
+        public int Fordulo { get; set; }
+        public int HazaiVeg { get; set; }
+        public int VegndegVeg { get; set; }
+        public int HazaigFelido { get; set; }
+        public int VendegFelido { get; set; }
+        public string HazaiCsapat { get; set; }
+        public string VendegCsapat { get; set; }
+
+    }
+}
