47e06ab3d76a81594a186da4766046f10c07ca68
kelemen.panna <kelemen.panna@BURG-1E05-17>
2024-09-16 08:51:26 +0200

    Tábla kapcsolat

--- FociWebApp/Models/FociDbContext.cs
+++ FociWebApp/Models/FociDbContext.cs
@@ -4,5 +4,9 @@ namespace FociWebApp.Models
 {
     public class FociDbContext:DbContext
     {
+        public FociDbContext(DbContextOptions<FociDbContext> options)
+            :base(options) {
+        }
+        public DbSet<Meccs> Meccsek {  get; set; }
     }
 }
