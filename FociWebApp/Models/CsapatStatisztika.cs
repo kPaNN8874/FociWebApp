56d74d1d525ba4786ae05c1b6df73f0ec55d2825
kelemen.panna <20.kelemen.panna@mechwart.com>
2024-09-19 11:50:54 +0200

    Bajnoksag allasa class lértehozása és fejlesztése

--- FociWebApp/Models/CsapatStatisztika.cs
+++ FociWebApp/Models/CsapatStatisztika.cs
@@ -5,10 +5,10 @@
         public string CsapatNev { get; set; }
         public int GyozelmekSzama { get; set; }
         public int DontetlenekSzama { get; set; }
-        public int VeresegekSzam { get; set; }
+        public int VeresegekSzama { get; set; }
         public int LottGolokSzama { get; set; }
         public int kapottGolokSzama { get; set; }
-        public int JatszottMeccsekSzama { get => GyozelmekSzama+DontetlenekSzama+VeresegekSzam set; }
-        public int GolKulonbseg { get =>  ; }
+        public int JatszottMeccsekSzama { get => GyozelmekSzama + DontetlenekSzama + VeresegekSzama;}
+        public int GolKulonbseg { get =>  LottGolokSzama-kapottGolokSzama; }
     }
 }
